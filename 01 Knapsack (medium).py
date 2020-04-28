5310c7336d5f89692cb65c8f3729f892fcf7075e
cl2333 <cl5293@nyu.edu>
2020-04-27 22:43:40 -0400

    update

--- 01 Knapsack (medium).py
+++ 01 Knapsack (medium).py
@@ -5,7 +5,7 @@ The goal is to get the maximum profit out of the items in the knapsack.
 Each item can only be selected once, as we don’t have multiple quantities of any item.
 
 Let’s take the example of Merry, who wants to carry some fruits in the knapsack to get maximum profit. 
-Here are the weights and profits of the fruits:
+    Here are the weights and profits of the fruits:
 
 Items: { Apple, Orange, Banana, Melon }
 Weights: { 2, 3, 1, 4 }
@@ -21,3 +21,46 @@ Banana + Melon (total weight 5) => 10 profit
 
 This shows that Banana + Melon is the best combination as it gives us the maximum profit and the total weight does not exceed the capacity.
 '''
+
+def solve_knapsack(profits, weights, capacity):
+  # basic checks
+  n = len(profits)
+  if capacity <= 0 or n == 0 or len(weights) != n:
+    return 0
+
+  dp = [0 for x in range(capacity+1)]
+
+  # if we have only one weight, we will take it if it is not more than the capacity
+  for c in range(0, capacity+1):
+    if weights[0] <= c:
+      dp[c] = profits[0]
+
+  # process all sub-arrays for all the capacities
+  for i in range(1, n):
+    for c in range(capacity, -1, -1):
+      profit1, profit2 = 0, 0
+      # include the item, if it is not more than the capacity
+      if weights[i] <= c:
+        profit1 = profits[i] + dp[c - weights[i]]
+      # exclude the item
+      profit2 = dp[c]
+      # take maximum
+      dp[c] = max(profit1, profit2)
+
+  return dp[capacity]
+
+
+def main():
+  print("Total knapsack profit: " +
+        str(solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 7)))
+  print("Total knapsack profit: " +
+        str(solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 6)))
+
+
+main()
+
+
+
+
+
+
